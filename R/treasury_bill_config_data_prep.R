2d55eec50dcb9489476348716cdd3118f977630c
Kapya <example@example.com>
2025-11-25 23:38:24 +0200

    updated tax rate

--- R/treasury_bill_config_data_prep.R
+++ R/treasury_bill_config_data_prep.R
@@ -4,7 +4,7 @@ treasury_bill_approx_interest_rates <- c(0.11, 0.12, 0.13, 0.14)
 treasury_bill_config <- data.frame(
   tenure = treasury_bill_tenures_days,
   interest_rate = treasury_bill_approx_interest_rates,
-  tax_rate = rep(0.15),
+  tax_rate = rep(0.20),
   handling_fee = rep(0.01)
 ) %>%
   mutate(
